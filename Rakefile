f050b8279019c6e580be769127b02467a9b33a42
Rob Sanheim <rsanheim@gmail.com>
2008-09-09 23:52:44 -0400

    fixin up manifest, rake, move to namespace, add html entites

--- Rakefile
+++ Rakefile
@@ -1,8 +1,12 @@
 require 'rubygems'
 require 'echoe'
-require './lib/gem_installer.rb'
+require './lib/rcr/gem_installer.rb'
 
-Echoe.new('rcr_gem_installer', GemTest::VERSION) do |p|
+Echoe.new('rcr_gem_installer', Rcr::GemInstaller::VERSION) do |p|
   p.rubyforge_name = 'rcr_gem_installer'
-  p.developer('Rob Sanheim', 'rob@runcoderun.com')
+  p.email = 'rob@runcoderun.com'
+  p.author = ["Rob Sanheim @ Relevance"]
+  p.summary = "Tool to install dependancies for RunCodeRun, though it coudl be used to bootstrap your own machines as well."
+  p.url = "http://runcoderun.com"
+  p.test_pattern = 'spec/**/*_spec.rb'
 end
--- spec/gem_installer_spec.rb
+++ spec/gem_installer_spec.rb
@@ -1,25 +1,26 @@
 require 'test/spec'
 
-require '../lib/gem_installer'
+require File.join(File.dirname(__FILE__), *%w[.. lib rcr gem_installer])
 
 describe 'GemInstaller' do
+  
   it "parses simple gem name" do
-    gems = GemInstaller.convert_gem_list("daemons (1.0.10)")
+    gems = Rcr::GemInstaller.convert_gem_list("daemons (1.0.10)")
     gems.first.name.should == "daemons"
   end
   
   it "parses name with dashes" do
-    gems = GemInstaller.convert_gem_list("diff-lcs (1.0.10)")
+    gems = Rcr::GemInstaller.convert_gem_list("diff-lcs (1.0.10)")
     gems.first.name.should == "diff-lcs"
   end
 
   it "parses name with underscores" do
-    gems = GemInstaller.convert_gem_list("spec_converter_foo (1.0.10)")
+    gems = Rcr::GemInstaller.convert_gem_list("spec_converter_foo (1.0.10)")
     gems.first.name.should == "spec_converter_foo"
   end
 
   it "parses with multiple versions" do
-    gems = GemInstaller.convert_gem_list("foo (1.0.10, 1.0.11)")
+    gems = Rcr::GemInstaller.convert_gem_list("foo (1.0.10, 1.0.11)")
     gems[0].version.should == "1.0.10"
     gems[1].version.should == "1.0.11"
   end

8bde1bf2c920f19bc6d54f27600037b1482e8838
Rob Sanheim <rsanheim@gmail.com>
2008-09-20 00:55:30 -0400

    version 0.2.0 - finish rename and stuff

--- lib/rcr/gem_sync.rb
+++ lib/rcr/gem_sync.rb
@@ -2,7 +2,7 @@ require 'ostruct'
 
 module Rcr
   class GemSync
-    VERSION = '0.1.4'
+    VERSION = '0.2.0'
     GITHUB = "http://gems.github.com"
     RCR_GEM_LIST = File.expand_path(File.join(File.dirname(__FILE__), *%w[.. runcoderun_gems.txt]))
   

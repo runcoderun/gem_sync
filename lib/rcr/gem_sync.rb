851c13aed160a41df238eb7ce7a6489dbc1a4982
Rob Sanheim <rsanheim@gmail.com>
2008-10-11 01:29:15 -0400

    push 0.4.1

--- lib/rcr/gem_sync.rb
+++ lib/rcr/gem_sync.rb
@@ -2,7 +2,7 @@ require 'ostruct'
 
 module Rcr
   class GemSync
-    VERSION = '0.4.0'
+    VERSION = '0.4.1'
     GITHUB = "http://gems.github.com"
     RCR_GEM_LIST = File.expand_path(File.join(File.dirname(__FILE__), *%w[.. runcoderun_gems.txt]))
   
